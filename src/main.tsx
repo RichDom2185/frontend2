ef0a686857deff01a62193a97c18254e3720d2e2
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-01-23 00:33:26 +0800

    Move global index styles to styles folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import App from './App.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import 'src/index.scss';
+import 'src/styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
