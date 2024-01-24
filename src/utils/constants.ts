b5ee67acdfe5e9d0f598bb88a5cb3bcd52025266
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-01-24 20:22:28 +0800

    Configure enabled languages via feature flags

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,10 +2,26 @@ const appName: string = import.meta.env.VITE_APP_NAME || '';
 const backendUrl: string = import.meta.env.VITE_BACKEND_URL || '';
 const sicpBackendUrl: string = import.meta.env.VITE_SICP_BACKEND_URL || '';
 
+// Feature flags
+const enableSource: boolean = import.meta.env.VITE_FEATURE_ENABLE_SOURCE === 'TRUE' || false;
+const enableFullJs: boolean = import.meta.env.VITE_FEATURE_ENABLE_FULL_JS === 'TRUE' || false;
+const enableFullTs: boolean = import.meta.env.VITE_FEATURE_ENABLE_FULL_TS === 'TRUE' || false;
+const enableHtml: boolean = import.meta.env.VITE_FEATURE_ENABLE_HTML === 'TRUE' || false;
+const enablePython: boolean = import.meta.env.VITE_FEATURE_ENABLE_PYTHON === 'TRUE' || false;
+const enableScheme: boolean = import.meta.env.VITE_FEATURE_ENABLE_SCHEME === 'TRUE' || false;
+
 const Constants = Object.freeze({
   appName,
   backendUrl,
   sicpBackendUrl,
+  featureFlags: {
+    enableSource,
+    enableFullJs,
+    enableFullTs,
+    enableHtml,
+    enablePython,
+    enableScheme,
+  },
 });
 
 export default Constants;
--- src/utils/languages/index.ts
+++ src/utils/languages/index.ts
@@ -1,3 +1,30 @@
+import Constants from '../constants';
+import { pyLanguages } from './python';
+import { schemeLanguages } from './scheme';
+import { fullJsLanguage, fullTsLanguage, htmlLanguage, sourceLanguages } from './source';
+import { LanguageConfig } from 'src/types/languages';
+
+const enabledLanguages: LanguageConfig[] = [];
+
+if (Constants.featureFlags.enableSource) {
+  enabledLanguages.push(...sourceLanguages);
+}
+
+if (Constants.featureFlags.enableFullJs) {
+  enabledLanguages.push(fullJsLanguage);
+}
+if (Constants.featureFlags.enableFullTs) {
+  enabledLanguages.push(fullTsLanguage);
+}
+if (Constants.featureFlags.enableHtml) {
+  enabledLanguages.push(htmlLanguage);
+}
+
+export const allLanguages = enabledLanguages.concat(
+  Constants.featureFlags.enablePython ? pyLanguages : [],
+  Constants.featureFlags.enableScheme ? schemeLanguages : []
+);
+
 // Re-export all languages
 export * from './source';
 export * from './python';
