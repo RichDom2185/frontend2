6ab7228d47307adb8fc8319ad8ccae89391d652c
RichDom2185 <RichDom2185@users.noreply.github.com>
2024-01-31 16:56:42 +0000

    deploy: 5d0ead0bef42622454975bf3903850c67a332a5c

---
+++ assets/allPaths-31tnCQuV.js
@@ -0,0 +1,1 @@
+import{I as n}from"./index-7lLlfVks.js";import{I as e}from"./index-KsG0Chs-.js";import{bh as t,I as s}from"./hooks-vRkVkpp6.js";import"./index-SIpXG5dO.js";function p(o,a){var r=t(o);return a===s.STANDARD?n[r]:e[r]}function f(o){return t(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,p as getIconPaths,f as iconNameToPathsRecordKey};
