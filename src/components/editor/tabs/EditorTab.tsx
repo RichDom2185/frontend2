d6ed5452105077055f69f85437f0ea3ef29d1ab7
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-01-31 23:00:51 +0800

    Migrate editor tab components from old frontend
    * Copy over components and utilities
    * Update imports and fix type issues
    * Standardize code format
    
    Functionality and styles will be ported over in a future commit.

---
+++ src/components/editor/tabs/EditorTab.tsx
@@ -0,0 +1,32 @@
+import { Card, Icon } from '@blueprintjs/core';
+import classNames from 'classnames';
+import React from 'react';
+
+type Props = {
+  filePath: string;
+  isActive: boolean;
+  setActive: () => void;
+  remove: () => void;
+};
+
+const EditorTab: React.FC<Props> = ({ filePath, isActive, setActive, remove }) => {
+  const onClick = (e: React.MouseEvent<HTMLElement>) => {
+    // Stop the click event from propagating to the parent component.
+    e.stopPropagation();
+    remove();
+  };
+
+  return (
+    <Card
+      className={classNames('editor-tab', {
+        selected: isActive,
+      })}
+      onClick={setActive}
+    >
+      {filePath}
+      <Icon className="remove-button" icon="small-cross" onClick={onClick} />
+    </Card>
+  );
+};
+
+export default EditorTab;
