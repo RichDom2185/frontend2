2c1f8cd4afd8e102dbaf1042ac143c84a6aa3738
RichDom2185 <RichDom2185@users.noreply.github.com>
2024-02-07 06:39:15 +0000

    deploy: b78f5b68a582f1e05f8039e4afd0b360390fb23f

---
+++ assets/allPathsLoader-cUG7JmUz.js
@@ -0,0 +1,7 @@
+import{_ as a}from"./index-8gRA3UwX.js";import{bd as i,be as n}from"./NavigationBar-nfb9R6nL.js";var c=function(e,o){return i(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,a(()=>import("./allPaths-cn-zQkKi.js"),__vite__mapDeps([0,1,2,3,4,5,6]))];case 1:return t=r.sent().getIconPaths,[2,t(e,o)]}})})};export{c as allPathsLoader};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/allPaths-cn-zQkKi.js","assets/index-7lLlfVks.js","assets/index-KsG0Chs-.js","assets/NavigationBar-nfb9R6nL.js","assets/index-8gRA3UwX.js","assets/index-mEJQ8Gqr.css","assets/NavigationBar-uXr_50wW.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
---
+++ assets/splitPathsBySizeLoader-JgtYoSZx.js
@@ -0,0 +1,7 @@
+import{_ as i}from"./index-8gRA3UwX.js";import{bd as s,be as _,bf as c,I as u}from"./NavigationBar-nfb9R6nL.js";var v=function(o,n){return s(void 0,void 0,void 0,function(){var a,r;return _(this,function(e){switch(e.label){case 0:return a=c(o),n!==u.STANDARD?[3,2]:[4,i(()=>import("./index-7lLlfVks.js").then(t=>t.I),__vite__mapDeps([]))];case 1:return r=e.sent(),[3,4];case 2:return[4,i(()=>import("./index-KsG0Chs-.js").then(t=>t.I),__vite__mapDeps([]))];case 3:r=e.sent(),e.label=4;case 4:return[2,r[a]]}})})};export{v as splitPathsBySizeLoader};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
