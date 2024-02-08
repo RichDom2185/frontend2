42332d38413f45c54e4aedf55f8192d073eebd0b
RichDom2185 <RichDom2185@users.noreply.github.com>
2024-02-08 07:46:43 +0000

    deploy: 4b70fc93df87db966e75161452dabd57c47acfe9

---
+++ assets/allPathsLoader-0Kp-4M3M.js
@@ -0,0 +1,7 @@
+import{_ as a}from"./index-TgPeFvTi.js";import{bd as i,be as n}from"./NavigationBar-jBpdOSXw.js";var c=function(e,o){return i(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,a(()=>import("./allPaths-vrAaKkH1.js"),__vite__mapDeps([0,1,2,3,4,5,6]))];case 1:return t=r.sent().getIconPaths,[2,t(e,o)]}})})};export{c as allPathsLoader};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/allPaths-vrAaKkH1.js","assets/index-7lLlfVks.js","assets/index-KsG0Chs-.js","assets/NavigationBar-jBpdOSXw.js","assets/index-TgPeFvTi.js","assets/index-mEJQ8Gqr.css","assets/NavigationBar-uXr_50wW.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
