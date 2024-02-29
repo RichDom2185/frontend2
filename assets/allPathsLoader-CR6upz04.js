705e787b67c23bb56ef8475086971ca274349976
RichDom2185 <RichDom2185@users.noreply.github.com>
2024-02-29 17:49:31 +0000

    deploy: c3a4e0f5cab8d6298c955d09c9d3e5811ea29548

---
+++ assets/allPathsLoader-CR6upz04.js
@@ -0,0 +1,7 @@
+import{_ as a}from"./index-CxBJfJvX.js";import{be as i,bf as n}from"./NavigationBar-rj9E88eF.js";var c=function(e,o){return i(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,a(()=>import("./allPaths-DhseQey-.js"),__vite__mapDeps([0,1,2,3,4,5,6]))];case 1:return t=r.sent().getIconPaths,[2,t(e,o)]}})})};export{c as allPathsLoader};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/allPaths-DhseQey-.js","assets/index-vLFZomIa.js","assets/index-CYBrJnPY.js","assets/NavigationBar-rj9E88eF.js","assets/index-CxBJfJvX.js","assets/index-Cs48vdQq.css","assets/NavigationBar-C5ev_nTB.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
---
+++ assets/splitPathsBySizeLoader-CW_sxADa.js
@@ -0,0 +1,7 @@
+import{_ as i}from"./index-CxBJfJvX.js";import{be as s,bf as _,bg as c,I as u}from"./NavigationBar-rj9E88eF.js";var v=function(o,n){return s(void 0,void 0,void 0,function(){var a,r;return _(this,function(e){switch(e.label){case 0:return a=c(o),n!==u.STANDARD?[3,2]:[4,i(()=>import("./index-vLFZomIa.js").then(t=>t.I),__vite__mapDeps([]))];case 1:return r=e.sent(),[3,4];case 2:return[4,i(()=>import("./index-CYBrJnPY.js").then(t=>t.I),__vite__mapDeps([]))];case 3:r=e.sent(),e.label=4;case 4:return[2,r[a]]}})})};export{v as splitPathsBySizeLoader};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
