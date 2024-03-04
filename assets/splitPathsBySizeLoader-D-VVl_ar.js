996b6b947a93e2841ac2f56a22bf8df9506d2d0f
RichDom2185 <RichDom2185@users.noreply.github.com>
2024-03-04 23:07:44 +0000

    deploy: 5a4c917ceb23d516ad4ed5be7d5d8995b1173a07

--- assets/splitPathsBySizeLoader-D-VVl_ar.js
+++
@@ -1,7 +0,0 @@
-import{_ as i}from"./index-DDLjFsow.js";import{bd as s,be as _,bf as c,I as u}from"./NavigationBar-BxSYH3yi.js";var v=function(o,n){return s(void 0,void 0,void 0,function(){var a,r;return _(this,function(e){switch(e.label){case 0:return a=c(o),n!==u.STANDARD?[3,2]:[4,i(()=>import("./index-BIdTI090.js").then(t=>t.I),__vite__mapDeps([]))];case 1:return r=e.sent(),[3,4];case 2:return[4,i(()=>import("./index-ClTF5qbq.js").then(t=>t.I),__vite__mapDeps([]))];case 3:r=e.sent(),e.label=4;case 4:return[2,r[a]]}})})};export{v as splitPathsBySizeLoader};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
