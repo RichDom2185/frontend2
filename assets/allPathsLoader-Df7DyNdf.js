44233cab8ce045d5c203f749ddb329a8a10e515f
RichDom2185 <RichDom2185@users.noreply.github.com>
2024-03-08 20:35:02 +0000

    deploy: 38b8937d8bdf05653b34724f4500df6fb760158f

---
+++ assets/allPathsLoader-Df7DyNdf.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/allPaths-OFiCPoFT.js","assets/index-BIdTI090.js","assets/index-ClTF5qbq.js","assets/NavigationBar-UbtBIpsx.js","assets/index-CPMdZOWr.js","assets/index-CZQWe8C3.css","assets/NavigationBar-C5ev_nTB.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as a}from"./index-CPMdZOWr.js";import{bd as i,be as n}from"./NavigationBar-UbtBIpsx.js";var c=function(e,o){return i(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,a(()=>import("./allPaths-OFiCPoFT.js"),__vite__mapDeps([0,1,2,3,4,5,6]))];case 1:return t=r.sent().getIconPaths,[2,t(e,o)]}})})};export{c as allPathsLoader};
