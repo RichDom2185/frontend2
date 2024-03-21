41ecd7e4883d00941b61456a344594db9c2764ca
RichDom2185 <RichDom2185@users.noreply.github.com>
2024-03-21 09:35:40 +0000

    deploy: 48facfc84e2ad0a1c735493da0f43dd673c7d95d

---
+++ assets/allPathsLoader-Dm8LLw5N.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/allPaths-DoxRzTHo.js","assets/index-Z15_5sD4.js","assets/index-Blrmf0ux.js","assets/NavigationBar-BJseC1nH.js","assets/index-BtYgzjY-.js","assets/index-Dkklo7uj.css","assets/NavigationBar-C5ev_nTB.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as a}from"./index-BtYgzjY-.js";import{bd as i,be as n}from"./NavigationBar-BJseC1nH.js";var c=function(e,o){return i(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,a(()=>import("./allPaths-DoxRzTHo.js"),__vite__mapDeps([0,1,2,3,4,5,6]))];case 1:return t=r.sent().getIconPaths,[2,t(e,o)]}})})};export{c as allPathsLoader};
