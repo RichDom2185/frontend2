e5d12125ebffb4e6cbb834d8a8190fa711271a74
RichDom2185 <RichDom2185@users.noreply.github.com>
2024-03-21 09:19:33 +0000

    deploy: 2e7f73739013a0337a17e9fb4e9e6df415e4e5d8

--- assets/allPathsLoader-CyfaLetE.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/allPaths-DNw8T8kF.js","assets/index-BIdTI090.js","assets/index-ClTF5qbq.js","assets/NavigationBar-0kcyYUcA.js","assets/index-BjlJ4WSM.js","assets/index-CZQWe8C3.css","assets/NavigationBar-C5ev_nTB.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as a}from"./index-BjlJ4WSM.js";import{bd as i,be as n}from"./NavigationBar-0kcyYUcA.js";var c=function(e,o){return i(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,a(()=>import("./allPaths-DNw8T8kF.js"),__vite__mapDeps([0,1,2,3,4,5,6]))];case 1:return t=r.sent().getIconPaths,[2,t(e,o)]}})})};export{c as allPathsLoader};
