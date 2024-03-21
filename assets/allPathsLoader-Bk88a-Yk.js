fba25adbe884c1ea80f89c78da05ae27a3c55a93
RichDom2185 <RichDom2185@users.noreply.github.com>
2024-03-21 09:30:44 +0000

    deploy: 4504e6a8a221fc0762c4fd4ec422ffd735dcd1d7

---
+++ assets/allPathsLoader-Bk88a-Yk.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/allPaths-gQjpoFjF.js","assets/index-BIdTI090.js","assets/index-ClTF5qbq.js","assets/NavigationBar-iVby6ROe.js","assets/index-KZ6gFI_m.js","assets/index-CZQWe8C3.css","assets/NavigationBar-C5ev_nTB.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as a}from"./index-KZ6gFI_m.js";import{bd as i,be as n}from"./NavigationBar-iVby6ROe.js";var c=function(e,o){return i(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,a(()=>import("./allPaths-gQjpoFjF.js"),__vite__mapDeps([0,1,2,3,4,5,6]))];case 1:return t=r.sent().getIconPaths,[2,t(e,o)]}})})};export{c as allPathsLoader};
--- assets/allPathsLoader-qutHjCUC.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/allPaths-BTBDBFCl.js","assets/index-BIdTI090.js","assets/index-ClTF5qbq.js","assets/NavigationBar-CCJEdw3p.js","assets/index-CR4iIMc9.js","assets/index-CZQWe8C3.css","assets/NavigationBar-C5ev_nTB.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as a}from"./index-CR4iIMc9.js";import{bd as i,be as n}from"./NavigationBar-CCJEdw3p.js";var c=function(e,o){return i(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,a(()=>import("./allPaths-BTBDBFCl.js"),__vite__mapDeps([0,1,2,3,4,5,6]))];case 1:return t=r.sent().getIconPaths,[2,t(e,o)]}})})};export{c as allPathsLoader};
