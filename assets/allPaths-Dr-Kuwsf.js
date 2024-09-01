6b9e921bd442823af007f7e0d6d1a725eba7af83
RichDom2185 <RichDom2185@users.noreply.github.com>
2024-09-01 03:56:39 +0000

    deploy: 800dbc4b01ee5f7b5f13ce179c9a34d9acacc9f2

--- assets/allPaths-Dr-Kuwsf.js
+++
@@ -1,1 +0,0 @@
-import{I as n}from"./index-IS1g_n7Q.js";import{I as e}from"./index-CvqEe05e.js";import{bf as t,I as s}from"./NavigationBar-DDUrlqLM.js";import"./index-i2e-YAUl.js";function f(o,a){var r=t(o);return a===s.STANDARD?n[r]:e[r]}function p(o){return t(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,f as getIconPaths,p as iconNameToPathsRecordKey};
