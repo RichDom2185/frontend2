9e975084a2b2b78299107a8038908b6319416815
RichDom2185 <RichDom2185@users.noreply.github.com>
2025-02-23 10:34:25 +0000

    deploy: 622973b5c4bc5e04b775a4284930115e17f6f299

--- assets/allPaths-BTBIbS2s.js
+++
@@ -1,1 +0,0 @@
-import{I as n}from"./index-DxLGmC6o.js";import{I as e}from"./index-CsS_OkTl.js";import{bj as t,I as s}from"./NavigationBar-BYUtP3YW.js";import"./index-Bwb6yVm7.js";function p(o,a){var r=t(o);return a===s.STANDARD?n[r]:e[r]}function f(o){return t(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,p as getIconPaths,f as iconNameToPathsRecordKey};
