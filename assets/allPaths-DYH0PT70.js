8d4b417ac727690321deba6704e0c499695cf68e
RichDom2185 <RichDom2185@users.noreply.github.com>
2025-02-26 00:20:00 +0000

    deploy: 6406e89b273f87a25ae4301cbe81f23dded84c65

---
+++ assets/allPaths-DYH0PT70.js
@@ -0,0 +1,1 @@
+import{I as n}from"./index-DxLGmC6o.js";import{I as e}from"./index-CsS_OkTl.js";import{bj as t,I as s}from"./NavigationBar-BKyp2qj9.js";import"./index-Ds53Cpz9.js";function p(o,a){var r=t(o);return a===s.STANDARD?n[r]:e[r]}function f(o){return t(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,p as getIconPaths,f as iconNameToPathsRecordKey};
