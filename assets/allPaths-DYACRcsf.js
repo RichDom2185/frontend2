3fe5e3d6e2f2ddd468e19422f85aaf23125b0eb6
RichDom2185 <RichDom2185@users.noreply.github.com>
2025-03-23 09:25:24 +0000

    deploy: ae3a276d37ca7851bb170ee5dd88291d3f8048ff

---
+++ assets/allPaths-DYACRcsf.js
@@ -0,0 +1,1 @@
+import{I as n}from"./index-DxLGmC6o.js";import{I as e}from"./index-CsS_OkTl.js";import{bj as t,I as s}from"./NavigationBar-4S5BastL.js";import"./index-CtlOFlct.js";function p(o,a){var r=t(o);return a===s.STANDARD?n[r]:e[r]}function f(o){return t(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,p as getIconPaths,f as iconNameToPathsRecordKey};
