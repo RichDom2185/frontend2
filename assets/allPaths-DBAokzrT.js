aa0bd37913997d3397ac94b5df18ef947bbe211e
RichDom2185 <RichDom2185@users.noreply.github.com>
2025-03-31 09:24:00 +0000

    deploy: 7c17f374160ccd3b37b5391b10ccfb23e69db043

---
+++ assets/allPaths-DBAokzrT.js
@@ -0,0 +1,1 @@
+import{I as n}from"./index-CMnAzps8.js";import{I as e}from"./index-Bt95j2vb.js";import{bm as t,I as s}from"./NavigationBar-mw7ZpMYK.js";import"./index-CJTuAJIu.js";function p(o,a){var r=t(o);return a===s.STANDARD?n[r]:e[r]}function f(o){return t(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,p as getIconPaths,f as iconNameToPathsRecordKey};
