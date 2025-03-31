a55c586608da130f9268f83c62b7597b3e55f40d
RichDom2185 <RichDom2185@users.noreply.github.com>
2025-03-31 09:18:57 +0000

    deploy: 94486e825b654f3e369b8417161e548e7e56230a

--- assets/allPaths-CEbTx3Vz.js
+++
@@ -1,1 +0,0 @@
-import{I as n}from"./index-CMnAzps8.js";import{I as e}from"./index-Bt95j2vb.js";import{bm as t,I as s}from"./NavigationBar-D-IxjOLL.js";import"./index-DgB3tZQH.js";function p(o,a){var r=t(o);return a===s.STANDARD?n[r]:e[r]}function f(o){return t(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,p as getIconPaths,f as iconNameToPathsRecordKey};
---
+++ assets/allPaths-Yz7tuQwc.js
@@ -0,0 +1,1 @@
+import{I as n}from"./index-CMnAzps8.js";import{I as e}from"./index-Bt95j2vb.js";import{bm as t,I as s}from"./NavigationBar-Cl4p5qPd.js";import"./index-BS_e1gtG.js";function p(o,a){var r=t(o);return a===s.STANDARD?n[r]:e[r]}function f(o){return t(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,p as getIconPaths,f as iconNameToPathsRecordKey};
