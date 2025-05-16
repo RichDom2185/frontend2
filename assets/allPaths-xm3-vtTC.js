f9f446d51df3195aea024391788a9c4a35126a06
RichDom2185 <RichDom2185@users.noreply.github.com>
2025-05-16 10:56:00 +0000

    deploy: 4b6bc985212abfc93c37a135177c74851cca4728

--- assets/allPaths-xm3-vtTC.js
+++
@@ -1,1 +0,0 @@
-import{I as n}from"./index-BKzgFDAn.js";import{I as e}from"./index-BvSuWPlB.js";import{bm as t,I as s}from"./NavigationBar-CsUmfepW.js";import"./index-zdwlUzlJ.js";function p(o,a){var r=t(o);return a===s.STANDARD?n[r]:e[r]}function f(o){return t(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,p as getIconPaths,f as iconNameToPathsRecordKey};
