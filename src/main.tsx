3f0a94c4287324d3f8a390dce2f0830f53d8beac
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-08-10 03:55:32 +0800

    Show focus on keyboard navigation only

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import App from './App.tsx';
 import { store } from './store';
 import { createInBrowserFileSystem } from './utils/fileSystem/createInBrowserFileSystem.ts';
+import { FocusStyleManager } from '@blueprintjs/core';
 import { Provider } from 'react-redux';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import 'src/styles/index.scss';
+
+FocusStyleManager.onlyShowFocusOnTabs();
 
 const root = createRoot(document.getElementById('root')!);
 
