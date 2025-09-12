af65166e0493c03028867e39c2471000138367ab
RichDom2185 <RichDom2185@users.noreply.github.com>
2025-09-12 13:51:04 +0000

    deploy: b176142b9369de54a0c4289fa8e38e481d14fd28

--- assets/allPaths-CbLUm33E.js
+++
@@ -1,1 +0,0 @@
-import{I as r}from"./index-CnPrfHYh.js";import{I as s}from"./index-Cnc1EkDo.js";import{a5 as n,I as c}from"./NavigationBar-CQG6ZVk5.js";import"./index-BiA9iHJM.js";function p(t,a){const o=n(t);return a===c.STANDARD?r[o]:s[o]}export{r as IconSvgPaths16,s as IconSvgPaths20,p as getIconPaths};
