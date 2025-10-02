3ebd9fe74b48d591da44172d4cca483910c1b039
RichDom2185 <RichDom2185@users.noreply.github.com>
2025-10-02 09:02:07 +0000

    deploy: 8ca96f760778fdf1a10451d96794ea2b0a8ec50e

--- assets/splitPathsBySizeLoader-6cAnS7O4.js
+++
@@ -1,1 +0,0 @@
-import{aI as e}from"./index-BAenOR_5.js";import{a5 as s,I as _}from"./NavigationBar-2ABDQT8F.js";const I=async(o,r)=>{const i=s(o);let t;return r===_.STANDARD?t=await e(()=>import("./index-CnPrfHYh.js").then(a=>a.I),[]):t=await e(()=>import("./index-Cnc1EkDo.js").then(a=>a.I),[]),t[i]};export{I as splitPathsBySizeLoader};
