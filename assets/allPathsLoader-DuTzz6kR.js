6239ca1d3e2541b68a34432cd2b11d7ce8cf9eea
RichDom2185 <RichDom2185@users.noreply.github.com>
2025-11-05 17:11:13 +0000

    deploy: d27003161240f583bed9e78e583cd9f12ab5089f

---
+++ assets/allPathsLoader-DuTzz6kR.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-C_HBB2a1.js","assets/index-DXdc2oG2.js","assets/index-BgzQ1XP9.js","assets/NavigationBar-BtigLibv.js","assets/index-CTTWBiRX.js","assets/index-TLZSEKzp.css","assets/NavigationBar-nL91hsG2.css"])))=>i.map(i=>d[i]);
+import{aH as e}from"./index-CTTWBiRX.js";const s=async(t,a)=>{const{getIconPaths:o}=await e(async()=>{const{getIconPaths:r}=await import("./allPaths-C_HBB2a1.js");return{getIconPaths:r}},__vite__mapDeps([0,1,2,3,4,5,6]));return o(t,a)};export{s as allPathsLoader};
