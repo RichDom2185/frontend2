8092c7fe07be51e8e1fd382db41b27784a70eb0b
RichDom2185 <RichDom2185@users.noreply.github.com>
2025-11-20 02:06:11 +0000

    deploy: 1e7ed7eb34429434b53f8654f16436fad81b65d7

---
+++ assets/allPathsLoader-CXFxiHV4.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-j8qLRn7T.js","assets/index-voJy5fZe.js","assets/index-BZITDwoa.js","assets/NavigationBar-C5yYjbki.js","assets/index-D29kqIJk.js","assets/index-DH24gUNc.css","assets/NavigationBar-nL91hsG2.css"])))=>i.map(i=>d[i]);
+import{aH as e}from"./index-D29kqIJk.js";const s=async(t,a)=>{const{getIconPaths:o}=await e(async()=>{const{getIconPaths:r}=await import("./allPaths-j8qLRn7T.js");return{getIconPaths:r}},__vite__mapDeps([0,1,2,3,4,5,6]));return o(t,a)};export{s as allPathsLoader};
