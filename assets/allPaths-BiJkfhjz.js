2066d5b91245c77f783419895df37c4e6646d5f7
RichDom2185 <RichDom2185@users.noreply.github.com>
2026-03-22 23:40:57 +0000

    deploy: e02fa795739709c6d9ef5c29699906d4b087e184

--- assets/allPaths-BiJkfhjz.js
+++
@@ -1,1 +0,0 @@
-import{I as r}from"./index-Bp4RqK05.js";import{I as s}from"./index-Uc4Xhv31.js";import{a3 as n,I as c}from"./NavigationBar-C_v1sgCF.js";import"./index-D3YhvPEw.js";function p(t,a){const o=n(t);return a===c.STANDARD?r[o]:s[o]}export{r as IconSvgPaths16,s as IconSvgPaths20,p as getIconPaths};
---
+++ assets/splitPathsBySizeLoader-DaZZuEVQ.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/paths-CKRsv3dt.js","assets/chunk-DECur_0Z.js","assets/paths-DTWyfBPH.js"])))=>i.map(i=>d[i]);
+import{t as e}from"./preload-helper-Bl9q33Ev.js";import{n as t,t as n}from"./iconTypes-mA9DF8iM.js";var r=async(r,i)=>{let a=t(r),o;return o=i===n.STANDARD?await e(()=>import(`./paths-CKRsv3dt.js`),__vite__mapDeps([0,1])):await e(()=>import(`./paths-DTWyfBPH.js`),__vite__mapDeps([2,1])),o[a]};export{r as splitPathsBySizeLoader};
