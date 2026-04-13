9e89d4fd23a5e437201d0d58dfb13fe3f61455a1
RichDom2185 <RichDom2185@users.noreply.github.com>
2026-04-13 13:23:16 +0000

    deploy: 8104a539250c02c34087bed914d09ea9c6c56155

--- assets/allPathsLoader-DbyW0PLr.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-BPKnA5Jf.js","assets/paths-C72eE_pa.js","assets/chunk-DECur_0Z.js","assets/paths-BnszXVpu.js","assets/iconTypes-mA9DF8iM.js"])))=>i.map(i=>d[i]);
-import{t as e}from"./preload-helper-Bl9q33Ev.js";var t=async(t,n)=>{let{getIconPaths:r}=await e(async()=>{let{getIconPaths:e}=await import(`./allPaths-BPKnA5Jf.js`);return{getIconPaths:e}},__vite__mapDeps([0,1,2,3,4]));return r(t,n)};export{t as allPathsLoader};
